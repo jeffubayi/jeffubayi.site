a0471922cb1626cd31aa23da8e848fc4f8dbc300
jeffubayi <jeffubayi@gmail.com>
2020-10-11 09:26:23 +0300

    Add 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ErrorImage from "../../static/404.png"
 
 import { Layout, SEO } from 'src/components'
 
@@ -7,7 +8,8 @@ const NotFoundPage = () => (
 		<SEO title="404: Not found" />
 		<h1>Not Found</h1>
 		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-	</Layout>
+		<img src={ErrorImage}></img>
+		</Layout>
 )
 
 export default NotFoundPage
